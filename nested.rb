2540f248fa7e1b05bc77a2819f49bb275f31846d
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-06-30 11:51:27 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+ programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,7 +81,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash[:yukihiro_matsumoto] = {
+  :yukihiro_matsumoto => {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+}
+}
 end
 
 def changing_alan
@@ -103,7 +108,12 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay] = {
+  :alan_kay => {
+        :known_for => "GUI",
+        :languages => ["Smalltalk", "LISP"]
+      }
+}
 end
 
 def adding_to_dennis
@@ -125,5 +135,11 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie] = {
+  :dennis_ritchie => {
+    :known_for => "Unix",
+    :languages => ["C" , "Assembly"]
+  }
+  
+}
 end
