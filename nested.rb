881e4e1e141d0320b467d0a2e2577e3fefb42c2d
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-06-30 11:50:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -135,5 +135,11 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:languages]= ["C" , "Assembly"]
+programmer_hash[:dennis_ritchie] = {
+  :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C" , "Assembly"]
+      }
+  
+}
 end
